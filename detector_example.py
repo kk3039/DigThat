79aac694007fc1bdb1f514395cdc9a3135df7b19
Victoria Lu <victoria@bigserene.com>
2020-10-22 16:00:39 -0400

    communications, probing and guessing

--- detector_example.py
+++ detector_example.py
@@ -1,49 +1,74 @@
 import getopt
 import json
+import math
+import random
 import socket
 import sys
-import random
 
-PLAYER_NAME = "example"
+PLAYER_NAME = 'example'
 DATA_SIZE = 4096
 
 
 def establish_connection(port):
-    HOST = "localhost"
+    HOST = 'localhost'
     PORT = port
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
     return s
 
 
-def receive_data(server):
+def receive_data(conn):
     while True:
-        data = server.recv(DATA_SIZE).decode()
+        data = conn.recv(DATA_SIZE).decode()
         if data:
-            return data
+            return json.loads(data)
 
 
-if __name__ == "__main__":
+def send_data(conn, data):
+    conn.sendall(json.dumps(data).encode())
+
+
+if __name__ == '__main__':
     optlist, args = getopt.getopt(sys.argv[1:], 'n:p:k:', [
         'grid=', 'phase=', 'tunnel=', 'port='])
     num_grid, num_phase, tunnel_length, port = 0, 0, 0, 8000
     for o, a in optlist:
-        if o in ("-n", "--grid"):
+        if o in ('-n', '--grid'):
             num_grid = int(a)
-        elif o in ("-p", "--phase"):
+        elif o in ('-p', '--phase'):
             num_phase = int(a)
-        elif o in ("-k", "--tunnel"):
+        elif o in ('-k', '--tunnel'):
             tunnel_length = int(a)
-        elif o in ("--port"):
+        elif o in ('--port'):
             port = int(a)
         else:
-            assert False, "unhandled option"
+            assert False, 'unhandled option'
 
-    server = establish_connection(port)
+    s = establish_connection(port)
     try:
         # Please fill in your team name here
-        server.sendall(f'{PLAYER_NAME}'.encode())
-        res = receive_data(server)
-        print(res)
-    except (KeyboardInterrupt, OSError, TypeError):
-        server.close()
+        send_data(s, {'player_name': PLAYER_NAME})
+        res = receive_data(s)
+        num_grid = res['grid']
+        num_phase = res['remaining_phases']
+        tunnel_length = res['tunnel_length']
+        while True:
+            payload = {'phase': 'probe', 'probes': []}
+            for i in range(3):
+                x = math.ceil(random.random() * (num_grid))
+                y = x = math.ceil(random.random() * (num_grid))
+                if [x, y] not in payload['probes']:
+                    # for the ease of decoding, please avoid using python tuples
+                    payload['probes'].append([x, y])
+            print("payload: {}".format(payload))
+            send_data(s, payload)
+            res = receive_data(s)
+            print(res)  # gets probing report
+            if res['next_phase'] == 'guess':
+                break
+        payload = {'phase': 'guess', 'answer': [
+            (1, 3), (2, 3), (3, 3), (4, 3), (5, 3)]}
+        send_data(s, payload)
+        s.close()
+    except KeyboardInterrupt:
+        s.close()
--- game.py
+++ game.py
@@ -4,6 +4,8 @@ import json
 import socket
 
 DATA_SIZE = 4096
+PROBE_PHASE = 'probe'
+GUESS_PHASE = 'guess'
 
 
 class Error(Exception):
@@ -21,7 +23,6 @@ class Detector:
     def __init__(self, name):
         self.name = name
         self.num_probe = 0
-        self.score = sys.maxint
 
     def update_probes(self, num_probe):
         self.num_probe += num_probe
@@ -32,20 +33,24 @@ class Game:
 
         if num_grid < 2:
             raise InputError(
-                num_grid, "grid size should be greater than or equal to 2")
+                num_grid, 'grid size should be greater than or equal to 2')
         if num_phase < 2:
             raise InputError(
-                num_phase, "number of phases should be greater or equal to 2")
+                num_phase, 'number of phases should be greater or equal to 2')
         if tunnel_length < num_grid - 1:
             raise InputError(
-                tunnel_length, "tunnel length should be at least n-1 for n * n grid")
+                tunnel_length, 'tunnel length should be at least n-1 for n * n grid')
 
         self.num_grid = num_grid
         self.num_phase = num_phase
         self.tunnel_length = tunnel_length
+        self.phase = PROBE_PHASE if num_phase > 2 else GUESS_PHASE
         self.intersections = [
             [0 for _ in range(self.num_grid+1)] for _ in range(self.num_grid+1)]
         self.intersection_neighbors = {}
+
+    def isEnded(self):
+        return self.num_phase <= 0
 
     def set_player(self, name):
         self.detector = Detector(name)
@@ -53,8 +58,22 @@ class Game:
     def use_probes(self, num_probes):
         self.detector.update_probes(num_probes)
 
+    def check_answer(self, answers):
+        self.num_phase -= 1
+        is_answer_correct = True
+        if len(answers) - 1 != tunnel_length:
+            is_answer_correct = False
+        for [x, y] in answers:
+            if self.intersections[x][y] != 1:
+                print("({},{}) is not part of the tunnel".format(x, y))
+                is_answer_correct = False
+        if not is_answer_correct:
+            print("answer is not correct")
+            return sys.maxsize
+        return self.detector.num_probe
+
     def load_tunnel(self):
-        f = open("tunnel", "r")
+        f = open('tunnel', 'r')
         prev = None
         route = []
 
@@ -66,7 +85,7 @@ class Game:
             route.append((x, y))
 
             if prev is not None and not abs(prev[0] - x) ^ abs(prev[1] - y):
-                raise InputError((x, y), "tunnel is not connected")
+                raise InputError((x, y), 'tunnel is not connected')
             self.intersections[x][y] = 1
             self.intersection_neighbors[(x, y)] = []
             # look for existing neighbors and set neighbor map
@@ -90,73 +109,121 @@ class Game:
                             (x + d_x, y + d_y))
 
             prev = (x, y)
-        print(route)
-        print(self.intersection_neighbors)
+        # print(route)
+        # print(self.intersection_neighbors)
 
         # validations
         if route[0][0] != 1:
-            assert False, "Starting point {} should be at index 1, i.e. (1,x)".format(
+            assert False, 'Starting point {} should be at index 1, i.e. (1,x)'.format(
                 route[0][0])
         if route[-1][0] != self.num_grid:
-            assert False, "Ending point {} should be at index {}, i.e. ({},x)".format(
+            assert False, 'Ending point {} should be at index {}, i.e. ({},x)'.format(
                 route[-1][0],  self.num_grid,  self.num_grid)
 
         if len(self.intersection_neighbors.get(route[0])) != 1:
-            assert False, "Starting point should only have one neighboring intersection."
+            assert False, 'Starting point should only have one neighboring intersection.'
         if len(self.intersection_neighbors.get(route[-1])) != 1:
-            assert False, "Ending point should only have one neighboring intersection."
+            assert False, 'Ending point should only have one neighboring intersection.'
         for i in route[2:-1]:
             if len(self.intersection_neighbors.get(i)) != 2:
-                assert False, "Intersection {} has only one or more than 2 neighbors. \
-                    Please check if it is dead end or part of a loop".format(route[i])
+                assert False, 'Intersection {} has only one or more than 2 neighbors. \
+                    Please check if it is dead end or part of a loop'.format(route[i])
         if len(route) - 1 > tunnel_length:
-            assert False, "tunnel route is of length {}, longer than the limit of {}".format(
+            assert False, 'tunnel route is of length {}, longer than the limit of {}'.format(
                 len(route), tunnel_length)
+
+    def investigate(self, probes):
+        self.num_phase -= 1
+        if self.num_phase == 1:
+            self.curr_phase = GUESS_PHASE
+
+        report = []
+        for [x, y] in probes:
+            if self.intersections[x][y] == 1:
+                report.append(
+                    {'[{},{}]'.format(x, y): self.intersection_neighbors.get((x, y))})
+
+        return report
+
+    def get_info(self, result=[]):
+        info = {'grid': self.num_grid, 'remaining_phases': self.num_phase,
+                'tunnel_length': self.tunnel_length, 'curr_phase': self.phase, 'result': result}
+        if self.num_phase > 1:
+            info['next_phase'] = PROBE_PHASE
+        elif self.num_phase == 1:
+            info['next_phase'] = GUESS_PHASE
+        return info
 
 
 def establish_connection(port):
-    HOST = "localhost"
+    HOST = 'localhost'
     PORT = port
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen(5)
-    return s
+    (conn, addr) = s.accept()
+    return conn
 
 
-if __name__ == "__main__":
+def receive_data(conn):
+    while True:
+        data = conn.recv(DATA_SIZE).decode()
+        if data:
+            return json.loads(data)
+
+
+def send_data(conn, data):
+    conn.sendall(json.dumps(data).encode())
+
+
+if __name__ == '__main__':
 
     optlist, args = getopt.getopt(sys.argv[1:], 'n:p:k:', [
         'grid=', 'phase=', 'tunnel=', 'port='])
     num_grid, num_phase, tunnel_length, port = 0, 0, 0, 8000
     for o, a in optlist:
-        if o in ("-n", "--grid"):
+        if o in ('-n', '--grid'):
             num_grid = int(a)
-        elif o in ("-p", "--phase"):
+        elif o in ('-p', '--phase'):
             num_phase = int(a)
-        elif o in ("-k", "--tunnel"):
+        elif o in ('-k', '--tunnel'):
             tunnel_length = int(a)
-        elif o in ("--port"):
+        elif o in ('--port'):
             port = int(a)
         else:
-            assert False, "unhandled option"
+            assert False, 'unhandled option'
 
     game = Game(num_grid, num_phase, tunnel_length)
     game.load_tunnel()
 
-    server = establish_connection(port)
+    conn = establish_connection(port)
     try:
-        (conn, address) = server.accept()
-        data = conn.recv(DATA_SIZE).decode()
+        data = receive_data(conn)
         if data:
-            game.set_player(data)
+            print(data)
+            game.set_player(data['player_name'])
+        # game info
 
-        payload = {'grid': num_grid, 'remaining_phases': num_phase,
-                   'tunnel_length': tunnel_length, 'state': 'probe', 'result': []}
-        conn.sendall("hello".encode())
+        payload = game.get_info()
+        send_data(conn, payload)
 
-    # while num_phase > 0:
-    #     data = json.loads(conn.recv(DATA_SIZE))
-    #     num_phase -= 1
-        server.close()
-    except (AttributeError, OSError, KeyboardInterrupt, RuntimeError, TypeError):
-        server.close()
+        # probing phase
+        # begin timer here
+        while not game.isEnded():
+            print("phase {}".format(num_phase - game.num_phase + 1))
+            req = receive_data(conn)
+            if game.num_phase > 1 and req['phase'] == PROBE_PHASE:
+                prob_result = game.investigate(req['probes'])
+                payload = game.get_info(prob_result)
+                send_data(conn, payload)
+            elif req['phase'] == GUESS_PHASE:
+                score = game.check_answer(req['answer'])
+                print("player {} got score {}".format(
+                    game.detector.name, score))
+            else:
+                assert False, "This is the guessing round"
+
+        conn.close()
+    except Error as e:
+        print(e)
+        conn.close()
